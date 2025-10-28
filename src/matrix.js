14c3bdac51e373701258d42ac082d55b29be1883
unknown <boy.bogdan03@gmail.com>
2025-10-29 01:50:46 +0200

    refactored matrix library

--- src/matrix.js
+++ src/matrix.js
@@ -1,60 +1,32 @@
 export function transpose(matrix) {
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  
-  const result = [];
-  for (let j = 0; j < cols; j++) {
-    result[j] = [];
-  }
-  
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      result[j][i] = matrix[i][j];
-    }
-  }
-  return result;
+  return matrix[0].map((_, colIndex) => 
+    matrix.map(row => row[colIndex])
+  );
 }
 
 export function add(m1, m2) {
-  const rows1 = m1.length;
-  const cols1 = m1[0].length;
-  const rows2 = m2.length;
-  const cols2 = m2[0].length;
-
-  if (rows1 !== rows2 || cols1 !== cols2) {
+  if (m1.length !== m2.length || m1[0].length !== m2[0].length) {
     throw new Error('Matrices must have the same dimensions for addition');
   }
 
-  const result = [];
-  for (let i = 0; i < rows1; i++) {
-    result[i] = [];
-    for (let j = 0; j < cols1; j++) {
-      result[i][j] = m1[i][j] + m2[i][j];
-    }
-  }
-  return result;
+  return m1.map((row, i) => 
+    row.map((value, j) => value + m2[i][j])
+  );
 }
 
 export function multiply(m1, m2) {
-  const rows1 = m1.length;
   const cols1 = m1[0].length;
   const rows2 = m2.length;
-  const cols2 = m2[0].length;
 
   if (cols1 !== rows2) {
     throw new Error('Column count of the first matrix must equal row count of the second');
   }
 
-  const result = [];
-  for (let i = 0; i < rows1; i++) {
-    result[i] = [];
-    for (let j = 0; j < cols2; j++) {
-      let sum = 0;
-      for (let k = 0; k < cols1; k++) {
-        sum += m1[i][k] * m2[k][j];
-      }
-      result[i][j] = sum;
-    }
-  }
-  return result;
+  const m2_transposed = transpose(m2);
+
+  return m1.map(row1 => 
+    m2_transposed.map(row2 =>
+      row1.reduce((sum, value, k) => sum + (value * row2[k]), 0)
+    )
+  );
 }
