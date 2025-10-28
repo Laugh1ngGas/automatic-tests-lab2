84d35b949b12e81f5ea6be6d1ca0267b78b98ee3
unknown <boy.bogdan03@gmail.com>
2025-10-29 01:44:07 +0200

    second green test (added add operation to matrix library)

--- src/matrix.js
+++ src/matrix.js
@@ -14,3 +14,23 @@ export function transpose(matrix) {
   }
   return result;
 }
+
+export function add(m1, m2) {
+  const rows1 = m1.length;
+  const cols1 = m1[0].length;
+  const rows2 = m2.length;
+  const cols2 = m2[0].length;
+
+  if (rows1 !== rows2 || cols1 !== cols2) {
+    throw new Error('Matrices must have the same dimensions for addition');
+  }
+
+  const result = [];
+  for (let i = 0; i < rows1; i++) {
+    result[i] = [];
+    for (let j = 0; j < cols1; j++) {
+      result[i][j] = m1[i][j] + m2[i][j];
+    }
+  }
+  return result;
+}
